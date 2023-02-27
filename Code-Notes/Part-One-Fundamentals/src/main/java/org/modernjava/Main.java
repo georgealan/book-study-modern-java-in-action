5ba0d826b7cbde342a1865b6140300c6088fb322
George Alan <georgealanrufo@gmail.com>
2023-02-26 22:15:01 -0300

    Continue in page 9

---
+++ Code-Notes/Part-One-Fundamentals/src/main/java/org/modernjava/Main.java
@@ -0,0 +1,11 @@
+package org.modernjava;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+/*
+TODO Continue in page 9
+ */
